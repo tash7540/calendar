c053e9e081488443f98734b02fa7155c924e3697
tash7540 <59746687+tash7540@users.noreply.github.com>
2022-08-25 19:53:09 -0600

    added Day component styling

--- src/components/calendar/day/day.js
+++ src/components/calendar/day/day.js
@@ -1,11 +1,7 @@
-import {useState,useEffect} from 'react';
-import moment from 'moment';
-import {yearOptions, monthOptions,weekArray,gridArray } from '../../../constants/index';
-import {Grid,Box,Card,Container} from '@material-ui/core';
-//import useStyles from './styles';
-
+import useStyles from './styles';
 
 const Day= ({year,month,day,events}) =>{
+    const classes = useStyles();
     const newMonth = month+1 <10 ? '0'+ String(month+1) : String(month+1);
     const time = year+'-'+newMonth+'-'+day;
     const data = events.filter( ( data ) => data.eventTime.slice(0,10) === time );
@@ -14,7 +10,7 @@ const Day= ({year,month,day,events}) =>{
   return(
       <>
       {data.map((x) =>
-        <div>{x.eventName} - {x.eventNote}</div>
+        <div className = {classes.container}>{x.eventName} - {x.eventNote}</div>
       )}
     <div>{day}</div>
     </>
---
+++ src/components/calendar/day/styles.js
@@ -0,0 +1,15 @@
+  
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+container:{
+
+    width: '100%',
+    color: '#fff',
+    fontSize:'10px',
+    background: '#000080',
+    height: '25px',
+    marginBottom: '3px',
+    },
+
+}));
